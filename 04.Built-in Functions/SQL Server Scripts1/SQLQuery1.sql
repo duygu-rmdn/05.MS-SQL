958e358b75a94ad7eef459ec15473492adcfdd8e
unknown <duygumetin99@gmail.com>
2021-01-25 23:40:34 +0200

    Adde

--- 04.Built-in Functions/SQL Server Scripts1/SQLQuery1.sql
+++ 04.Built-in Functions/SQL Server Scripts1/SQLQuery1.sql
@@ -37,3 +37,14 @@ SELECT EmployeeID,	FirstName,	LastName,	Salary,
 	FROM Employees
 	WHERE Salary BETWEEN 10000 AND 50000 
 	ORDER BY Salary DESC
+
+SELECT * FROM
+(SELECT EmployeeID,	FirstName,	LastName,	Salary,
+	DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+	FROM Employees
+	WHERE Salary BETWEEN 10000 AND 50000 
+) AS Ranked
+WHERE [Rank] = 2
+	ORDER BY Salary DESC
+		
+

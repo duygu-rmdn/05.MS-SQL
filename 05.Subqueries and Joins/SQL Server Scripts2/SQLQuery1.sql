72b8a74f2ed865c3d49a7a388abe487a95abb0bf
unknown <duygumetin99@gmail.com>
2021-02-07 23:54:47 +0200

    Added

--- 05.Subqueries and Joins/SQL Server Scripts2/SQLQuery1.sql
+++ 05.Subqueries and Joins/SQL Server Scripts2/SQLQuery1.sql
@@ -14,3 +14,16 @@ SELECT c.CountryCode, COUNT(*)
 	WHERE c.CountryCode IN ('US', 'RU', 'BG')
 	GROUP BY c.CountryCode
 
+SELECT TOP(5) crs.CountryName, r.RiverName
+	FROM Countries AS crs 
+	LEFT JOIN Continents AS cts ON crs.ContinentCode = cts.ContinentCode
+	LEFT OUTER JOIN CountriesRivers AS cr ON crs.CountryCode = cr.CountryCode
+	LEFT OUTER JOIN Rivers AS r ON cr.RiverId = r.Id
+	WHERE cts.ContinentName = 'Africa'
+	ORDER BY crs.CountryName ASC
+
+SELECT co.ContinentCode, cou.CurrencyCode
+	FROM Continents co
+	JOIN Countries cou on co.ContinentCode = cou.ContinentCode
+	JOIN Currencies cu ON cu.CurrencyCode = cou.CurrencyCode
+	ORDER BY

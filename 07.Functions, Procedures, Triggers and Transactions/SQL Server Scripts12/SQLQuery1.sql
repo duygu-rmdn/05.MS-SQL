14587e4ee1602026ccf461fcfbd5f54712c7130a
Duygu <duygumetin99@gmail.com>
2021-02-10 18:43:20 +0200

    Added

---
+++ 07.Functions, Procedures, Triggers and Transactions/SQL Server Scripts12/SQLQuery1.sql
@@ -0,0 +1,29 @@
+USE Bank
+GO
+
+CREATE TABLE Logs
+(
+	LogId INT IDENTITY PRIMARY KEY,
+	AccountId INT NOT NULL,
+	OldSum DECIMAL(18,4) NOT NULL,
+	NewSum DECIMAL(18,4) NOT NULL
+)
+GO
+
+CREATE TRIGGER tr_OnAccoutChangesAddLogRecord
+ON Accounts FOR UPDATE
+AS
+	INSERT INTO Logs(AccountId, OldSum, NewSum)
+		SELECT i.Id, d.Balance, i.Balance
+			FROM inserted AS i
+			JOIN deleted AS d ON i.Id = d.Id
+GO
+
+UPDATE Accounts
+	SET Balance += 10000
+	WHERE Id BETWEEN 3 AND 6
+GO
+
+SELECT *
+	FROM Logs
+GO
